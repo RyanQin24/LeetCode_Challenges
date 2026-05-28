0ff39a951c4b72e8ce63788860c408e361905cbc
RyanQin24 <148712489+RyanQin24@users.noreply.github.com>
2026-05-28 15:43:17 -0400

    Add files via upload

---
+++ Ez/Remove_Duplicates.cpp
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        while(i < n-1){
+            if(nums[i] == nums[i+1]){
+                for(int j = i; j < n-1; j++){
+                    nums[j] = nums[j+1];
+                }
+                n--;
+            }else{
+                i++;
+            }
+        }
+        return n;
+    }
+};
