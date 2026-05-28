b5196b815772d2832d6995e520432ceb79c4d5fa
RyanQin24 <148712489+RyanQin24@users.noreply.github.com>
2026-05-28 14:57:47 -0400

    Add files via upload

---
+++ Ez/2_Sorted_Lists.cpp
@@ -0,0 +1,24 @@
+#https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head = list1; 
+        ListNode* retval = list1; 
+        ListNode** header = &head;
+        while(list2 != nullptr){
+            if((*header) == nullptr ||(list2->val) < ((*header)->val)){
+                ListNode* temp = list2;
+                list2 = list2->next;
+                temp->next = *header; 
+                if(*header == retval){
+                    retval = temp;
+                }
+                *header = temp;
+            }else{
+                header = &((*header)->next);
+            }
+        }
+        return retval;
+    }
+};
